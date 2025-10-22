a2e9d08e61386653e0dffa159a73f960354b43ea
briansil1 <brian.a.sil@gmail.com>
2025-10-21 21:46:19 -0600

    Todos los cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,7 +40,7 @@
                         <input type="hidden" class="form-control" id="user_locale_hidden" aria-label="user_locale_hidden" aria-describedby="user_locale_hidden" value="{{ app()->getLocale() }}">
                        </div>
                     <div class="back-blue text-center">
-                        <h2 class="h1 uppercase text-2xl md:text-5xl text-white container oswald">{{ __('main.content.profiles') }} <br> {{ __('main.content.profiles-2') }}</h2>
+                        <h2 class="h1 uppercase text-2xl md:text-5xl text-white container oswald">{{ __('dynamic.content.profiles') }} <br> {{ __('main.content.profiles-2') }}</h2>
                         <h3 class="text-white mt-0 pt-0 oswald">
                             <label id="tool_continent"  class="text-white mt-0 pt-0 oswald" aria-label="tool_continent" aria-describedby="tool_continent">{{ $continent_name }}</label>
                         </h3>
